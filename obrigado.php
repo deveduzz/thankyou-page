e7647a2abd1d24257f47e7b8f5a54b8e0a281c63
Eduzz <jefferson@eduzz.com>
2016-01-20 16:57:42 -0200

    Adicionado novos parametros
    { Nome do comprador , Nome do afiliado , E-mail do Afiliado }

--- obrigado.php
+++ obrigado.php
@@ -1,5 +1,7 @@
 <?php
 	$afiliado      = (isset($_POST['afiliado']) && is_numeric($_POST['afiliado'])) ? $_POST['afiliado'] : 0;
+	$nomeafiliado  = (isset($_POST['nomeafiliado']) && is_numeric($_POST['nomeafiliado'])) ? $_POST['nomeafiliado'] : 0;
+	$emailafiliado = (isset($_POST['emailafiliado']) && is_numeric($_POST['emailafiliado'])) ? $_POST['emailafiliado'] : 0;
 	$fatura        = (isset($_POST['fatura'])   && is_numeric($_POST['fatura'])) ? $_POST['fatura'] : 0;
 	$utm_source    = (isset($_POST['utm_souce'])) ? $_POST['utm_source'] : '';
 	$utm_campaign  = (isset($_POST['utm_campaign'])) ? $_POST['utm_campaign'] : '';
@@ -9,6 +11,7 @@
 	$pxvlfb        = (isset($_POST['pxvlfb'])  && is_numeric($_POST['pxvlfb'])) ? $_POST['pxvlfb'] : '';
 	$produto       = (isset($_POST['produto']) && is_numeric($_POST['produto'])) ? $_POST['produto'] : 0;
 	$eml_comprador = (isset($_POST['email_comprador'])) ? $_POST['email_comprador'] : '';
+	$nome_comprador= (isset($_POST['nome_comprador'])) ? $_POST['nome_comprador'] : '';
 
 	if( $afiliado == '815959' )
 	{
